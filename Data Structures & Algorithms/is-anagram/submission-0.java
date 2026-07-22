b0e2660e1459d663f534ec020d871c4b0537bb7b
Bhavith Madhu <bhavithmadhu@gmail.com>
2026-07-22 14:40:33 +0530

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        // Count characters from s
+        for (char ch : s.toCharArray()) {
+
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) + 1);
+            } else {
+                map.put(ch, 1);
+            }
+        }
+
+        // Remove characters using t
+        for (char ch : t.toCharArray()) {
+
+            if (!map.containsKey(ch)) {
+                return false;
+            }
+
+            map.put(ch, map.get(ch) - 1);
+
+            if (map.get(ch) == 0) {
+                map.remove(ch);
+            }
+        }
+
+        return map.isEmpty();
+    }
+    }
+
